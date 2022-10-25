7438b3eb28eb92245413bb03ef6b4ac4ca0a5605
SachinGadge13 <gadges14@gmail.com>
2022-10-25 14:58:04 +0530

    Added:Program to read and write Employee Payroll to a Console.

--- src/main/java/com/bridgelabz/EmployeePayrollService/Day_27_EmployeePayroll/EmployeePayroll.java
+++
@@ -1,9 +0,0 @@
-package com.bridgelabz.EmployeePayrollService.Day_27_EmployeePayroll;
-
-public class EmployeePayroll {
-	public static void main(String[] args) {
-		System.out.println("Welome to Employee Payroll Service..");
-
-	}
-
-}
